c55b38e93a1d7079413cd4f602b504d0f3a69826
infinixs5lite <kelvinchinedu97@gmail.com>
2024-09-17 23:19:35 +0100

    Update and rename omni_moon.mk to twrp_moon.mk

--- twrp_moon.mk
+++ twrp_moon.mk
@@ -7,23 +7,22 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from moon device
 $(call inherit-product, device/xiaomi/moon/device.mk)
 
 PRODUCT_DEVICE := moon
-PRODUCT_NAME := omni_moon
+PRODUCT_NAME := twrp_moon
 PRODUCT_BRAND := Redmi
-PRODUCT_MODEL := moon
+PRODUCT_MODEL := poco m6
 PRODUCT_MANUFACTURER := xiaomi
+PRODUCT_RELEASE_NAME := moon
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="vnd_moon-user 12 SP1A.210812.016 V816.0.4.0.UNTIDXM release-keys"
-
-BUILD_FINGERPRINT := Redmi/vnd_moon/moon:12/SP1A.210812.016/V816.0.4.0.UNTIDXM:user/release-keys
