f1882b52a9fdd61d0f359279d50a516f6d756732
infinixs5lite <kelvinchinedu97@gmail.com>
2024-09-17 23:20:08 +0100

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,10 +1,1 @@
-#
-# Copyright (C) 2024 The Android Open Source Project
-# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
 
-add_lunch_combo omni_moon-user
-add_lunch_combo omni_moon-userdebug
-add_lunch_combo omni_moon-eng
